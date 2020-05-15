fe5f5c1a27829b8b9d98f55772ae9194e3e7afd9
117503445 <t117503445@gmail.com>
2020-05-15 15:47:19 +0800

    修复 Docker容器中无法发送广播包

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,7 @@
 docker build -t 117503445/wake_on_lan_server .
 docker rm wake_on_lan_server -f
-docker run --name wake_on_lan_server -p 8004:80 -d --restart always  117503445/wake_on_lan_server
+docker run --name wake_on_lan_server --network host -d --restart always  117503445/wake_on_lan_server
+
+docker run --name wake_on_lan_server  -p 8004:80 -d --restart always  117503445/wake_on_lan_server
 
 docker push 117503445/wake_on_lan_server
