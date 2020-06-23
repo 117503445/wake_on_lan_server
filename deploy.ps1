7cbcbc97d6c08192aa6727c4981cebd864b479e7
117503445 <t117503445@gmail.com>
2020-06-23 16:11:21 +0800

    update deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,3 @@
-docker build -t 117503445/wake_on_lan_server .
 docker rm wake_on_lan_server -f
+docker rmi 117503445/wake_on_lan_server
 docker run --name wake_on_lan_server --network host -d --restart always  117503445/wake_on_lan_server
-
-docker run --name wake_on_lan_server  -p 8004:80 -d --restart always  117503445/wake_on_lan_server
-
-docker push 117503445/wake_on_lan_server
