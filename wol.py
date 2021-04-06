c4ffbaac008f8ac650e446a3ad6d5c26df853739
117503445 <t117503445@gmail.com>
2021-04-06 19:05:40 +0800

    Update wol.py

--- wol.py
+++ wol.py
@@ -8,9 +8,7 @@ def wake_on_lan():
     js = json.loads(js)
     mac = js['mac']
     broadcast = js['broadcast']
-    print(mac, broadcast)
     send_magic_packet(mac, ip_address=broadcast)
-    send_magic_packet('2c:f0:5D:21:54:1e', ip_address='192.168.31.255')
 
 
 
